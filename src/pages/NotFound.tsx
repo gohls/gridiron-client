f55290734e64fc50aa6ebd348bc5bfed0fe935e2
Simon Gohl <sagohl@gmail.com>
2024-11-13 11:17:56 -0800

    update style

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,8 +3,8 @@ return (
     <>
     <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-        <p className="text-base font-semibold">404</p>
-        <h1 className="mt-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+        <p className="text-5xl sm:text-7xl font-semibold">404</p>
+        <h1 className="mt-4 text-balance font-semibold tracking-tight text-gray-900 ">
             The page you are looking for does not exist.
         </h1>
         </div>
