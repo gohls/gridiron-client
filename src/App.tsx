9201138f1024555d64c22d3232144b6eb18b04a1
Simon Gohl <sagohl@gmail.com>
2024-11-17 19:32:43 -0800

    add crypto

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,17 @@ import Header from '@/components/layout/Header';
 import AxiosInterceptor from '@/components/AxiosInterceptor';
 import AppRoute from '@/routes/AppRoute';
 
-
 const App = () => {
   return (
     <Router>
       <AuthProvider>
-      <AxiosInterceptor /> 
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <AppRoute />
-        </main>
-      </div>
+        <AxiosInterceptor />
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">
+            <AppRoute />
+          </main>
+        </div>
       </AuthProvider>
     </Router>
   );
