bdb2e0287b828f2b08b6c4d232d1ae6ce79dc3c4
Simon Gohl <sagohl@gmail.com>
2024-11-27 15:51:50 -0800

    small styling update

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Divider } from '@/components/tailwind/divider';
+import { Heading } from '@/components/tailwind/heading';
+
+const Home = () => {
+  return (
+    <>
+      <Heading>Home</Heading>
+      <Divider />
+    </>
+  );
+};
+
+export default Home;
