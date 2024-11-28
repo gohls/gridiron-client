bdaaa4f4c967af0be2d36c3323b3ec4f5c6a1400
Simon Gohl <sagohl@gmail.com>
2024-11-27 16:00:29 -0800

    component clean-up

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,9 +1,7 @@
 import NavigationMenu from '@/components/navigation/NavigationMenu';
 
 const Header = () => {
-    return <NavigationMenu />
-  };
+  return <NavigationMenu />;
+};
 
-
-  export default Header;
-
+export default Header;
